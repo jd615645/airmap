22148c30dea2623c416eedcd18c904424d0bd4d9
jd615645 <jd615645@gmail.com>
2016-05-22 01:52:41 +0800

    :cat2:add LASS & EPA site

--- public/js/airmap.js
+++ public/js/airmap.js
@@ -1,61 +1,82 @@
 var dust2_5_NASA_gap = [0, 3, 5, 8, 10, 13, 15, 18, 20, 35, 50, 65];
-var color_icon = ['./img/cdn0.png',
-																			'./img/cdn1.png',
-																			'./img/cdn2.png',
-																			'./img/cdn3.png',
-																			'./img/cdn4.png',
-																			'./img/cdn5.png',
-																			'./img/cdn6.png',
-																			'./img/cdn7.png',
-																			'./img/cdn8.png',
-																			'./img/cdn9.png',
-																			'./img/cdn10.png',
-																			'./img/cdn11.png',
-																			'./img/cdn12.png']
-var windytyInit = {
-	// Required: API key
-	key: 'PsL-At-XpsPTZexBwUkO7Mx5I',
 
-	// Optional: Initial state of the map
-	lat: 23.854271,
-	lon: 120.951906,
-	zoom: 8,
+var windytyInit = {
+  // Required: API key
+  key: 'PsL-At-XpsPTZexBwUkO7Mx5I',
+
+  // Optional: Initial state of the map
+  lat: 23.854271,
+  lon: 120.951906,
+  zoom: 8,
 }  
 
+var html = '<table width="100%"><tbody><tr><td>空氣溫度</td><td>-- °C</td></tr><tr><td>相對濕度</td><td>-- %</td></tr><tr><td>PM2.5</td><td>-- μg/m<sup>3</sup></td></tr></tbody></table>';
 // Required: Windyty main function is called after 
 // initialization of API
 //
 // @map is instance of Leaflet maps
 //
 function windytyMain(map) {
-	$.getJSON('./json/LASS_last.json', function(data) {
-			$.each(data, function(ik, iv) {
-				// console.log(iv);
-				var  SiteName = iv.SiteName,
-						pm25 = iv.RawData.s_d0,
-						lat = iv.RawData.gps_lat,
-						lng = iv.RawData.gps_lon;
- 
-				$.each(dust2_5_NASA_gap, function(jk, jv) {
-					 /* iterate through array or object */
-				});
-
-				L.marker([lat, lng], {icon: color(pm25)})
-					.bindPopup(SiteName).openPopup()
-					.addTo(map);
-			});
-	});
+  // LASS
+  $.getJSON('./json/LASS_last.json', function(data) {
+      $.each(data, function(ik, iv) {
+        // console.log(iv);
+        var siteName = iv.SiteName,
+            siteType = iv.SiteGroup,
+            channelId = '',
+            pm25 = iv.Data.Dust2_5,
+            humidity = iv.Data.Humidity,
+            temperature = iv.Data.Temperature,
+            last_time = iv.Data.Create_at,
+            lat = iv.LatLng.lat,
+            lng = iv.LatLng.lng;
+        L.marker([lat, lng], {icon: color(siteType, pm25)})
+         .bindPopup(info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time)).openPopup()
+         .addTo(map);
+      });
+  });
+  $.getJSON('./json/EPA_last.json', function(data) {
+      $.each(data, function(ik, iv) {
+        // console.log(iv);
+        var siteName = iv.SiteName,
+            siteType = iv.SiteGroup,
+            channelId = iv.Channel_id,
+            pm25 = iv.Data.Dust2_5,
+            humidity = '--',
+            temperature = '--',
+            last_time = iv.Data.Create_at,
+            lat = iv.LatLng.lat,
+            lng = iv.LatLng.lng;
+        L.marker([lat, lng], {icon: color(siteType, pm25)})
+         .bindPopup(info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time)).openPopup()
+         .addTo(map);
+      });
+  });
 }
 
-function color(pm25) {
-	var num = 0;
-	$.each(dust2_5_NASA_gap, function(key, val) {
-		if(pm25 >= val)
-			num = key;
-	});
-	return L.icon({iconUrl: color_icon[num]});
+function color(siteType, pm25) {
+  var num = 0;
+  $.each(dust2_5_NASA_gap, function(key, val) {
+    if(pm25 >= val)
+      num = key;
+  });
+  if(siteType == 'EPA')
+    return L.icon({iconUrl: './img/ddn' + num + '.png', iconSize: [19, 19],});
+  return L.icon({iconUrl: './img/cdn' + num + '.png', iconSize: [19, 19],});
 }
-
+  
+function info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time) {
+  var url;
+  switch(siteType) {
+    case 'LASS':
+      url = 'http://nrl.iis.sinica.edu.tw/LASS/show.php?device_id=' + siteName;
+      break;
+    case 'EPA':
+      url = 'https://thingspeak.com/channels/' + channelId;
+      break;
+  }
+  return '<div class="info-window"><a href="' + url + '" target="_blank"><h4>' + siteName + '</h4></a><p class="last_time">資料時間 ' + last_time + '</p><table><tbody><tr><td>PM2.5</td><td>' + pm25 + ' μg/m<sup>3</sup></td></tr><tr><td>空氣溫度</td><td>'+ temperature +' °C</td></tr><tr><td>相對濕度</td><td>'+ humidity +' %</td></tr></tbody></table></div>';
+}
 
 // PM25 s_d0
 // temperature s_t0
