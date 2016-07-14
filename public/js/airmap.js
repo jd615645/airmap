62d562f1ca2ae254298941a090ad9d88bf8d25b1
jd615645 <jd615645@gmail.com>
2016-07-14 13:20:03 +0800

    :cat2:add marker type layer

--- public/js/airmap.js
+++ public/js/airmap.js
@@ -1,13 +1,16 @@
 var pm25_gap = [0, 11, 23, 35, 41, 47, 53, 58, 64, 70],
-    pm25_gap_color = ['#c9e7a7', '#00ff00', '#0c0', '#ff0', '#f3c647', '#e46c0a', '#d99694', '#ff0000', '#800000', '#7030a0'],
-    pm25_NASA_gap = [0, 3, 5, 8, 10, 13, 15, 18, 20, 35, 50, 65],
-    pm25_NASA_gap_color = ['#0000cc', '#03c', '#06f', '#09f', '#3cf', '#6f9', '#9f6', '#cf3', '#ff0', '#ff9833', '#ff3300', '#f30', '#c00', '#800000'],
-    temp_gap = [0, 5, 10, 15, 20, 25, 30, 35, 40],
-    temp_gap_color = ['#215968', '#b7dee8', '#77933c', '#d7e4bd', '#fac090', '#e46c0a', '#ff0000', '#800000']
-    humi_gap = [20, 40, 60, 80],
+    pm25_gap_color = ['#c9e7a7', '#00ff00', '#0c0', '#ff0', '#f3c647', '#e46c0a', '#d99694', '#ff0000', '#800000', '#7030a0'];
+var pm25_NASA_gap = [0, 3, 5, 8, 10, 13, 15, 18, 20, 35, 50, 65],
+    pm25_NASA_gap_color = ['#0000cc', '#03c', '#06f', '#09f', '#3cf', '#6f9', '#9f6', '#cf3', '#ff0', '#ff9833', '#ff3300', '#f30', '#c00', '#800000'];
+var temp_gap = [0, 5, 10, 15, 20, 25, 30, 35, 40],
+    temp_gap_color = ['#215968', '#b7dee8', '#77933c', '#d7e4bd', '#fac090', '#e46c0a', '#ff0000', '#800000'];
+var humi_gap = [20, 40, 60, 80],
     humi_gap_color = ['#fac090', '#76b531', '#b7dee8', '#215968'];
-
+var air_group = ['LASS', 'EPA', 'Indie'];
 var marker_view = 1;
+var marker = {'LASS': [], 'EPA': [], 'Indie': []};
+var leaflet_map;
+var air_site = [];
 
 var windytyInit = {
   // Required: API key
@@ -26,7 +29,7 @@ var html = '<table width="100%"><tbody><tr><td>空氣溫度</td><td>-- °C</td><
 // @map is instance of Leaflet maps
 //
 function windytyMain(map) {
-
+  leaflet_map = map;
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     opacity: 0.3
@@ -49,68 +52,81 @@ function windytyMain(map) {
   // W.on('redrawFinished',function( displayedParams ) {
   //   state.innerHTML = new Date( displayedParams.timestamp ).toString();
   // },'test')
+  
+  $.ajax({
+      url: "http://g0vairmap.3203.info/Data/ProbeCube_last.json",
+   
+      // The name of the callback parameter, as specified by the YQL service
+      jsonp: "callback",
+   
+      // Tell jQuery we're expecting JSONP
+      dataType: "jsonp",
+   
+      // Tell YQL what we want and that we want JSON
+      data: {
+          q: "select title,abstract,url from search.news where query=\"cat\"",
+          format: "json"
+      },
+   
+      // Work with the response
+      success: function( response ) {
+          console.log( response ); // server response
+      },
+      fail: function(  ) {
+          console.log( 'fail' ); // server response
+      }
+  });
 
-  var LASS_marker = [], EPA_marker = [];
-
-  // LASS
-  $.getJSON('./json/LASS_last.json', function(data) {
-    $.each(data, function(key, val) {
-      var siteName = val.SiteName,
-          siteType = val.SiteGroup,
-          channelId = (val.Channel_id === undefined ? '' : val.Channel_id),
-          pm25 = val.Data.Dust2_5,
-          humidity = val.Data.Humidity,
-          temperature = val.Data.Temperature,
-          last_time = val.Data.Create_at,
-          lat = val.LatLng.lat,
-          lng = val.LatLng.lng;
-      // LASS_marker[key] = L.circleMarker([lat, lng], {color: markerColor(pm25)}).bindPopup(info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time));
-      LASS_marker[key] = L.circleMarker([lat, lng], {color: markerColor(pm25)});
-    });
-    $.each(LASS_marker, function(key, val) {
-      val.addTo(map);
+  $.each(air_group, function(ik, iv) {
+    $.getJSON('./json/' + iv + '_last.json', function(data) {
+      air_site[iv] = [];
+      $.each(data, function(jk, jv) {
+        var siteName = jv.SiteName,
+            siteType = jv.SiteGroup,
+            channelId = (jv.Channel_id === undefined ? '' : jv.Channel_id),
+            pm25 = (jv.Data.Dust2_5  === undefined ? '-' : jv.Data.Dust2_5),
+            humidity = (jv.Data.Humidity === undefined ? '-' : jv.Data.Humidity),
+            temperature = (jv.Data.Temperature === undefined ? '-' : jv.Data.Temperature),
+            last_time = jv.Data.Create_at,
+            lat = jv.LatLng.lat,
+            lng = jv.LatLng.lng;
+        air_site[iv][jk] = {siteName: siteName, siteType: siteType, channelId: channelId, pm25: pm25, humidity: humidity, temperature: temperature, last_time: last_time, lat: lat, lng: lng};
+        marker[iv][jk] = L.circleMarker([lat, lng], {color: markerColor(pm25)})
+                          .bindPopup(info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time));
+      });
+      $.each(marker[iv], function(key, val) {
+        val.addTo(map);
+      });
     });
   });
-  $.getJSON('./json/EPA_last.json', function(data) {
-    $.each(data, function(key, val) {
-      var siteName = val.SiteName,
-          siteType = val.SiteGroup,
-          channelId = (val.Channel_id === undefined ? '' : val.Channel_id),
-          pm25 = val.Data.Dust2_5,
-          humidity = (val.Data.Humidity === undefined ? '-' : val.Data.Humidity),
-          temperature = (val.Data.Temperature === undefined ? '-' : val.Data.Temperature),
-          last_time = val.Data.Create_at,
-          lat = val.LatLng.lat,
-          lng = val.LatLng.lng;
-
-      EPA_marker[key] = L.circleMarker([lat, lng], {color: markerColor(pm25)});
-    });
-    $.each(EPA_marker, function(key, val) {
-      val.addTo(map);
-    });
-  });
-  // $.getJSON('./json/Indie_last.json', function(data) {
-  //   $.each(data, function(ik, iv) {
-  //     // console.log(iv);
-  //     var siteName = iv.SiteName,
-  //         siteType = iv.SiteGroup,
-  //         channelId = iv.Channel_id,
-  //         pm25 = iv.Data.Dust2_5,
-  //         humidity = '--',
-  //         temperature = '--',
-  //         last_time = iv.Data.Create_at,
-  //         lat = iv.LatLng.lat,
-  //         lng = iv.LatLng.lng;
-  //     // console.log(lat + ', ' + lng);
-  //     L.marker([lat, lng], {icon: color(siteType, pm25)})
-  //      .bindPopup(info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time)).openPopup()
-  //      .addTo(map);
-  //   });
-  // });
 }
 
 $('#type-menu button').click(function() {
-  marker_view = $(this).attr('value');
+  marker_view = parseInt($(this).attr('value'));
+  switch (marker_view) {
+    case 1:
+    case 2:
+      $.each(air_group, function(ik, iv) {
+        $.each(marker[iv], function(jk, jv) {
+          jv.setStyle({color: markerColor(air_site[iv][jk].pm25)});
+        });
+      });
+      break;
+    case 3:
+      $.each(air_group, function(ik, iv) {
+        $.each(marker[iv], function(jk, jv) {
+          jv.setStyle({color: markerColor(air_site[iv][jk].temperature)});
+        });
+      });
+      break;
+    case 4:
+      $.each(air_group, function(ik, iv) {
+        $.each(marker[iv], function(jk, jv) {
+          jv.setStyle({color: markerColor(air_site[iv][jk].humidity)});
+        });
+      });
+      break;
+  }
 });
 
 function markerColor(data) {
@@ -141,6 +157,8 @@ function markerColor(data) {
       });
       break;
   }
+  if (data == null) color = '#000';
+
   return color;
 }
 
@@ -156,6 +174,7 @@ function color(siteType, pm25) {
 }
 
 function info_html(siteName, siteType, channelId, pm25, humidity, temperature, last_time) {
+  if (pm25 == null) pm25 = '-';
   var url;
   switch(siteType) {
     case 'LASS':
@@ -165,6 +184,7 @@ function info_html(siteName, siteType, channelId, pm25, humidity, temperature, l
       url = 'https://thingspeak.com/channels/' + channelId;
       break;
   }
+  
   return '<div class="info-window"><a href="' + url + '" target="_blank"><h4>' + siteName + '</h4></a><p class="last_time">資料時間 ' + last_time + '</p><table><tbody><tr><td>PM2.5</td><td>' + pm25 + ' μg/m<sup>3</sup></td></tr><tr><td>空氣溫度</td><td>'+ temperature +' °C</td></tr><tr><td>相對濕度</td><td>'+ humidity +' %</td></tr></tbody></table></div>';
 }
 
